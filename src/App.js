853e309e0b00ef70743a82492d5844311e2cbd2e
unknown <yeasin18801@gmail.com>
2022-10-29 01:32:54 +0600

    data-pass-child-to-parent

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
-import { Child } from "./component/state_lefting/Child";
 
+import { Child } from "./component/state_lefting/Child";
 
 function App() {
 const data = "I am Child Component";
+const handaleChildData = (childData) =>{
+  console.log(childData);
+}
+
   return (
     <div className="App">
       <h1>I am Parent Component </h1>
       {/* data pass parent to child component by props  */}
-      <Child data={data} />
+      <Child data={data} onChildData={handaleChildData} />
     </div>
   );
 }
--- src/component/state_lefting/Child.js
+++ src/component/state_lefting/Child.js
@@ -1,6 +1,8 @@
 import React from 'react'
 
 export const Child = (props) => {  // data receive by props 
+const data = "I am from Child ";
+props.onChildData(data);
   return (
     <div>
         <h1>{props.data}</h1>
