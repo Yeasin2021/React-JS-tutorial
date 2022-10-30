6b4150000b2bd14978942cdd478b6ba74e57d84d
unknown <yeasin18801@gmail.com>
2022-10-30 12:21:41 +0600

    todos-5

--- src/component/todos/NewTodo.js
+++ src/component/todos/NewTodo.js
@@ -1,13 +1,22 @@
 import React,{useState} from 'react'
 
 const NewTodo = () => {
-//   const [toDo,setToDo] = useState('');
+  const [toDo,setToDo] = useState('');
+  const handaleInputChange = (event) => {
+        setToDo(event.target.value);
+  }
+
+  const handaleDataPush = (event) => {
+        event.preventDefault();
+        console.log(toDo)
+  }
+
   return (
     // <p style={{ color:"red" }}>NewTodo</p>
-    <form>
-        <label htmlFor='input-one'>
-            <input type="text" id="todo" name="one" />
-        </label>
+    <form onSubmit={handaleDataPush}>
+        <label htmlFor='input-one'>NewTodo:</label>
+        <input type="text" id="todo" name="one" value={toDo} onChange={handaleInputChange}/>
+        <button type='submit'>Submit</button>
     </form>
   )
 }
