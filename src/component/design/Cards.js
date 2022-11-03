809f5ac7a8115d72dcf00a5357f292402c5faaae
unknown <yeasin18801@gmail.com>
2022-11-04 00:25:14 +0600

    cards-4

--- src/component/design/Cards.js
+++ src/component/design/Cards.js
@@ -6,35 +6,50 @@ const Cards = () => {
         <div className='container'>
             <div className='row'>
                 <div className='col-md-2 col-lg-4 col-sm-1'>
-                    <h1>Hello Grid System </h1>
-                    <p>
-                       With supporting text below as a natural lead-in to additional content.
-                       With supporting text below as a natural lead-in to additional content.
-                       With supporting text below as a natural lead-in to additional content.
-                       With supporting text below as a natural lead-in to additional content.
-                       With supporting text below as a natural lead-in to additional content.
-                    </p>
+                    <div className='card text-white bg-primary mb-3'>
+                        <div className='card-body'>
+                            <h1>Hello Grid System </h1>
+                            <p>
+                            With supporting text below as a natural lead-in to additional content.
+                            With supporting text below as a natural lead-in to additional content.
+                            With supporting text below as a natural lead-in to additional content.
+                            With supporting text below as a natural lead-in to additional content.
+                            With supporting text below as a natural lead-in to additional content.
+                            </p>
+                        </div>
+                    </div>
                 </div>
                 <div className='col-md-2 col-lg-4 col-sm-1'>
-                    <h1>Hello Grid System </h1>
-                    <p>
-                       With supporting text below as a natural lead-in to additional content.
-                       With supporting text below as a natural lead-in to additional content.
-                       With supporting text below as a natural lead-in to additional content.
-                       With supporting text below as a natural lead-in to additional content.
-                       With supporting text below as a natural lead-in to additional content.
-                    </p>
+                    <div className='card text-white bg-info mb-3'>
+                        <div className='card-body'>
+                            <h1>Hello Grid System </h1>
+                            <p>
+                            With supporting text below as a natural lead-in to additional content.
+                            With supporting text below as a natural lead-in to additional content.
+                            With supporting text below as a natural lead-in to additional content.
+                            With supporting text below as a natural lead-in to additional content.
+                            With supporting text below as a natural lead-in to additional content.
+                            </p>
+                        </div>
+                    </div>
                 </div>
                 <div className='col-md-2 col-lg-4 col-sm-1'>
-                    <h1>Hello Grid System </h1>
-                    <p>
-                       With supporting text below as a natural lead-in to additional content.
-                       With supporting text below as a natural lead-in to additional content.
-                       With supporting text below as a natural lead-in to additional content.
-                       With supporting text below as a natural lead-in to additional content.
-                       With supporting text below as a natural lead-in to additional content.
-                    </p>
+                    <div className='card text-white bg-secondary mb-3'>
+                        <div className='card-body'>
+                            <h1>Hello Grid System </h1>
+                            <p>
+                            With supporting text below as a natural lead-in to additional content.
+                            With supporting text below as a natural lead-in to additional content.
+                            With supporting text below as a natural lead-in to additional content.
+                            With supporting text below as a natural lead-in to additional content.
+                            With supporting text below as a natural lead-in to additional content.
+                            </p>
+                        </div>
+                    </div>
                 </div>
+
+                
+             
             </div>
         </div>
     </div>
