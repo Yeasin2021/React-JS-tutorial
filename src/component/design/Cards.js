3e876789a51ee33dc56e8da4e129cd367dbf8bba
unknown <yeasin18801@gmail.com>
2022-11-03 23:52:49 +0600

    cards-1

--- src/component/design/Cards.js
+++ src/component/design/Cards.js
@@ -3,7 +3,14 @@ import React from 'react'
 const Cards = () => {
   return (
     <div>
-      <h1>Hello Cards</h1>
+      <div class="card" style={{width: "18rem", "background": "white"}}>
+  <img class="card-img-top" src="..." alt="Card image cap" />
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
     </div>
   )
 }
