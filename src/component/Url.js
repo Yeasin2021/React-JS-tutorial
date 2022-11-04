1f8d3de10326b3cedaab959c4d0e8e3e6d6f96d3
unknown <yeasin18801@gmail.com>
2022-11-04 12:16:52 +0600

    welcome-screen

--- src/component/Url.js
+++ src/component/Url.js
@@ -7,12 +7,14 @@ import Login from './login/Login';
 import Child from './state_lefting/Child';
 import Parent from './state_lefting/Parent';
 import Home from './todos/Home';
+import Welcome from './Welcome';
 
  const Url = () => {
   return (
     <div>
         <BrowserRouter>
             <Routes>
+                <Route path='/' element={<Welcome />} />
                 <Route path='/login' element={<Login />} />
                 <Route path='/child' element={<Child />} />
                 <Route path='/parent' element={<Parent />} />
