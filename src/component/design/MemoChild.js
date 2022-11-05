d7a0c831c8f4a046aede7ab7e6880ebd06cda156
unknown <yeasin18801@gmail.com>
2022-11-05 16:45:48 +0600

    memo-child-rendering-remove-type-revers

--- src/component/design/MemoChild.js
+++ src/component/design/MemoChild.js
@@ -1,12 +1,21 @@
 import React, { memo } from 'react'
 
-const MemoChild = memo(() => {
+// const MemoChild = memo(() => {
+//     return (
+//       <div>
+//          {console.log('Child Rendering')}
+//       </div>
+//     )
+//   });
+//   export default MemoChild
+
+const MemoChild = () => {
     return (
       <div>
          {console.log('Child Rendering')}
       </div>
     )
-  });
+  };
 
-// export default memo(MemoChild)
-export default MemoChild
+export default memo(MemoChild)
+
