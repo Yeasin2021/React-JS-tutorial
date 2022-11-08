b8296773ac08446395c46e2497c6ebac1dd303d6
unknown <yeasin18801@gmail.com>
2022-11-08 13:15:47 +0600

    error-page-2

--- src/component/design/Error404.js
+++ src/component/design/Error404.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import './formInput.css';
 
 const Error404 = () => {
   return (
-    <div>
-      <h1 style={{ color:"red" }}>Page Not Found</h1>
-    </div>
+    <div id="notfound">
+		<div className="notfound">
+			<div className="notfound-404">
+				<h1>4<span>0</span>4</h1>
+			</div>
+			<h2>the page you requested could not found</h2>
+		</div>
+	</div>
   )
 }
 
