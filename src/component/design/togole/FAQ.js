81ea54d34e55a14b119809cf56684d26f8872c26
unknown <yeasin18801@gmail.com>
2022-11-14 16:39:42 +0600

    FAQ-10

--- src/component/design/togole/FAQ.js
+++ src/component/design/togole/FAQ.js
@@ -4,13 +4,23 @@ import { useState } from 'react'
 const FAQ = ({id,title,description}) => {
   const [toggle,setToggle] = useState(false);
   return (
-    <article>
-      <div>
-        <h1>{title}</h1>
-        <button onClick={() => {setToggle(!toggle)}}>{toggle ? '+':'-'}</button>
-        {toggle && <p>{description}</p>}
-      </div>
-    </article>
+    // <article>
+    //   <div>
+    //     <h1>{title}</h1>
+    //     <button onClick={() => {setToggle(!toggle)}}>{toggle ? '+':'-'}</button>
+    //     {toggle && <p>{description}</p>}
+    //   </div>
+    // </article>
+    <div className='container'>
+       <h1>Frequently Asked Questions</h1>
+        <details>
+            <summary>How do I create accordion?</summary>
+            <button onClick={() => {setToggle(!toggle)}}>{toggle ? '+':'-'}</button>
+            <div>
+            {toggle && <p>{description}</p>}
+            </div>
+        </details>
+    </div>
   )
 }
 
