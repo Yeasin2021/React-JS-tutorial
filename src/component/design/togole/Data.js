05e072ef8793296acb91798e6a7cc9e85cc3b4db
unknown <yeasin18801@gmail.com>
2022-11-14 14:39:39 +0600

    FAQ-3

---
+++ src/component/design/togole/Data.js
@@ -0,0 +1,19 @@
+export const FAQS = [
+
+    {
+        id : 1,
+        title : "title one",
+        description : "description one",
+    },
+    {
+        id : 2,
+        title : "title two",
+        description : "description two",
+    },
+    {
+        id : 3,
+        title : "titl three",
+        description : "description three",
+    }
+    
+]
