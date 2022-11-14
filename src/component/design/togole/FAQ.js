870017993c0820212843d6f3f50d53234adeecd8
unknown <yeasin18801@gmail.com>
2022-11-14 16:27:36 +0600

    FAQ-9

--- src/component/design/togole/FAQ.js
+++ src/component/design/togole/FAQ.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import { useState } from 'react'
 
 const FAQ = ({id,title,description}) => {
+  const [toggle,setToggle] = useState(false);
   return (
     <article>
       <div>
         <h1>{title}</h1>
-        <button>+</button>
-        <p>{description}</p>
+        <button onClick={() => {setToggle(!toggle)}}>{toggle ? '+':'-'}</button>
+        {toggle && <p>{description}</p>}
       </div>
     </article>
   )
