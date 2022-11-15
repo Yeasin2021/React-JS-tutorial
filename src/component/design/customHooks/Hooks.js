8fcc5382a27880fbf760c29fdd88d04cd10bbaf6
unknown <yeasin18801@gmail.com>
2022-11-15 19:06:37 +0600

    Custom-Hooks-3

--- src/component/design/customHooks/Hooks.js
+++ src/component/design/customHooks/Hooks.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Hooks = () => {
+  const [count,setCount] = useState(0);
   return (
     <div>
-        <h1> Hooks</h1>
+        <h1>Hooks</h1>
     </div>
   )
 }
