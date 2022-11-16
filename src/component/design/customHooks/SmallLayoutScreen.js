e0c7b2aa22312f2de1c1a51bd6c5a3f2b09c9239
unknown <yeasin18801@gmail.com>
2022-11-16 14:07:40 +0600

    custom hooks-3-declareScreenSize function

--- src/component/design/customHooks/SmallLayoutScreen.js
+++ src/component/design/customHooks/SmallLayoutScreen.js
@@ -3,7 +3,11 @@ import { useState } from 'react'
 
 const SmallLayoutScreen = () => {
   // tracking state by useState hooks
-  const [onSmallScreen,setonSmallScreen] = useState(false)
+  const [onSmallScreen,setOnSmallScreen] = useState(false);
+  const checkScreenSize = () =>{
+    setOnSmallScreen(window.innerWidth < 768);
+  }
+
   return (
     <div>
       <h1>This device is Small</h1>
