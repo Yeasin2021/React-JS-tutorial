f437f2793df80a83d8d72ee8481f47d1ac6d6a81
unknown <yeasin18801@gmail.com>
2022-11-16 14:03:08 +0600

    custom hooks-2-useState

--- src/component/design/customHooks/SmallLayoutScreen.js
+++ src/component/design/customHooks/SmallLayoutScreen.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useState } from 'react'
 
 const SmallLayoutScreen = () => {
+  // tracking state by useState hooks
+  const [onSmallScreen,setonSmallScreen] = useState(false)
   return (
     <div>
       <h1>This device is Small</h1>
