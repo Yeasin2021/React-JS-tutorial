c84d71373b93eefd82493391b6417ec362bde437
unknown <yeasin18801@gmail.com>
2022-11-17 12:19:30 +0600

    card body image setup

--- src/component/design/bootstrap-page/CardBody.js
+++ src/component/design/bootstrap-page/CardBody.js
@@ -6,13 +6,24 @@ const CardBody = () => {
     <div class="container text-center">
         <div class="row">
             <div class="col">
-                
+                <h5>
+                    Loream Ipsum
+                    <small className="text-muted" style={{ marginLeft:'10px' }}>Image</small>
+                </h5>
                <img src='https://picsum.photos/200/300' alt='one'/>
             </div>
             <div class="col">
+                <h5>
+                    Loream Ipsum
+                    <small className="text-muted" style={{ marginLeft:'10px' }}>Image</small>
+                </h5>
               <img src='https://picsum.photos/200/300' alt='two'/>
             </div>
             <div class="col">
+                <h5>
+                    Loream Ipsum
+                    <small className="text-muted" style={{ marginLeft:'10px' }}>Image</small>
+                </h5>
               <img src='https://picsum.photos/200/300' alt='three'/>
             </div>
         </div>
