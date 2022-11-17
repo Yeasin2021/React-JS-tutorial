d08f13d431f0b22bcdf2d695c6896194feadb313
unknown <yeasin18801@gmail.com>
2022-11-17 12:59:42 +0600

    card body image setup-body-finish

--- src/component/design/bootstrap-page/CardBody.js
+++ src/component/design/bootstrap-page/CardBody.js
@@ -7,30 +7,60 @@ const CardBody = () => {
         <div className="row">
             <div className="col">
                 <div className="card" style={{ width: "18rem;" }}>
-                    <img src="https://picsum.photos/200/300" className="card-img-top" alt="..." height='500px'/>
+                    <img src="https://picsum.photos/200/300" className="card-img-top" alt="..." height='300px'/>
                     <div className="card-body">
                         <h5 className="card-title">Card title</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <p className="card-text text-dark">What is Lorem Ipsum?
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
+                            standard dummy text ever since the 1500s, 
+                            when an unknown printer took a galley of 
+                            type and scrambled it to make a type specimen book. 
+                            It has survived not only five centuries, but also the 
+                            leap into electronic typesetting, remaining essentially unchanged.
+                            It was popularised in the 1960s with the release of Letraset sheets 
+                            containing Lorem Ipsum passages, and more 
+                            recently with desktop publishing
+                        </p>
                         <a href="#" className="btn btn-primary">Go somewhere</a>
                     </div>
                 </div>
             </div>
             <div className="col">
                 <div className="card" style={{ width: "18rem;" }}>
-                    <img src="https://picsum.photos/200/300" className="card-img-top" alt="..." height='500px'/>
+                    <img src="https://picsum.photos/200/300" className="card-img-top" alt="..." height='300px'/>
                     <div className="card-body">
                         <h5 className="card-title">Card title</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <p className="card-text text-dark">What is Lorem Ipsum?
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
+                            standard dummy text ever since the 1500s, 
+                            when an unknown printer took a galley of 
+                            type and scrambled it to make a type specimen book. 
+                            It has survived not only five centuries, but also the 
+                            leap into electronic typesetting, remaining essentially unchanged.
+                            It was popularised in the 1960s with the release of Letraset sheets 
+                            containing Lorem Ipsum passages, and more 
+                            recently with desktop publishing
+                        </p>
                         <a href="#" className="btn btn-primary">Go somewhere</a>
                     </div>
                 </div>
             </div>
             <div className="col">
                 <div className="card" style={{ width: "18rem;" }}>
-                    <img src="https://picsum.photos/200/300" className="card-img-top" alt="..." height='500px'/>
+                    <img src="https://picsum.photos/200/300" className="card-img-top" alt="..." height='300px'/>
                     <div className="card-body">
                         <h5 className="card-title">Card title</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <p className="card-text text-dark">What is Lorem Ipsum?
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
+                            standard dummy text ever since the 1500s, 
+                            when an unknown printer took a galley of 
+                            type and scrambled it to make a type specimen book. 
+                            It has survived not only five centuries, but also the 
+                            leap into electronic typesetting, remaining essentially unchanged.
+                            It was popularised in the 1960s with the release of Letraset sheets 
+                            containing Lorem Ipsum passages, and more 
+                            recently with desktop publishing
+                        </p>
                         <a href="#" className="btn btn-primary">Go somewhere</a>
                     </div>
                 </div>
