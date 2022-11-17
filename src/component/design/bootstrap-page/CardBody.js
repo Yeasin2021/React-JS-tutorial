015f9c9a51956471680f5c6ee0ff9f053dbd6174
unknown <yeasin18801@gmail.com>
2022-11-17 12:43:44 +0600

    card body image setup-body-image

--- src/component/design/bootstrap-page/CardBody.js
+++ src/component/design/bootstrap-page/CardBody.js
@@ -7,7 +7,7 @@ const CardBody = () => {
         <div className="row">
             <div className="col">
                 <div className="card" style={{ width: "18rem;" }}>
-                    <img src="..." className="card-img-top" alt="..." />
+                    <img src="https://picsum.photos/200/300" className="card-img-top" alt="..." height='500px'/>
                     <div className="card-body">
                         <h5 className="card-title">Card title</h5>
                         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -17,7 +17,7 @@ const CardBody = () => {
             </div>
             <div className="col">
                 <div className="card" style={{ width: "18rem;" }}>
-                    <img src="..." className="card-img-top" alt="..." />
+                    <img src="https://picsum.photos/200/300" className="card-img-top" alt="..." height='500px'/>
                     <div className="card-body">
                         <h5 className="card-title">Card title</h5>
                         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -27,7 +27,7 @@ const CardBody = () => {
             </div>
             <div className="col">
                 <div className="card" style={{ width: "18rem;" }}>
-                    <img src="..." className="card-img-top" alt="..." />
+                    <img src="https://picsum.photos/200/300" className="card-img-top" alt="..." height='500px'/>
                     <div className="card-body">
                         <h5 className="card-title">Card title</h5>
                         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
