2a8cef9e23717d7856a4a230be38b0fa2730b16a
unknown <yeasin18801@gmail.com>
2022-11-23 23:45:51 +0600

    Count Button function in title

--- src/component/design/tutorial/Count.js
+++ src/component/design/tutorial/Count.js
@@ -3,10 +3,14 @@ import { useState, useEffect } from 'react'
 
 const Count = () => {
 const [count,setCount] = useState(0);
+
+    document.title = `Click ${count} times`;
+
   return (
     <div>
       <h1 style={{ color:"red" }}>You Click {count} times</h1>
       <button className='btn btn-primary' onClick={() => setCount(count+1)}>Click</button>
+      {/* <button className='btn btn-primary' onClick={Click}>Click</button> */}
     </div>
   )
 }
