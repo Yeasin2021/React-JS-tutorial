ec7d7d0b3e260be83df0c27f5412c786d947feee
unknown <yeasin18801@gmail.com>
2022-11-23 23:31:55 +0600

    Count Button

--- src/component/design/tutorial/Count.js
+++ src/component/design/tutorial/Count.js
@@ -2,10 +2,11 @@ import React from 'react'
 import { useState, useEffect } from 'react'
 
 const Count = () => {
-
+const [count,setCount] = useState(0);
   return (
     <div>
       <h1>Count</h1>
+      <button className='btn btn-primary'>Click</button>
     </div>
   )
 }
