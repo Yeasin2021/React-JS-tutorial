01fb90498e06603f9a37dbf799bd8bd44430d9b8
unknown <yeasin18801@gmail.com>
2022-11-24 17:07:12 +0600

    Slider Three

--- src/component/design/slider/Slider.js
+++ src/component/design/slider/Slider.js
@@ -1,9 +1,45 @@
 import React from 'react'
+import ReactDOM from "react-dom";
+import { useRef, useState } from 'react';
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination, Navigation } from "swiper";
+import "swiper/css/bundle";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import "./slider.css";
 
 const slider = () => {
   return (
-    <div>
-      <h1 style={{ color:"red" }}>React Slider Component </h1>
+    // <div>
+    //   <h1 style={{ color:"red" }}>React Slider Component </h1>
+    // </div>
+    <div style={{ marginTop: '50px' }}>
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
     </div>
   )
 }
