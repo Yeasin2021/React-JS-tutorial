64e2759770a9a6c2e768f158ea068a9bb46c3e19
unknown <yeasin18801@gmail.com>
2022-12-02 17:42:21 +0600

    data fetch api 5

--- src/component/fetch_curd/data.js
+++ src/component/fetch_curd/data.js
@@ -15,29 +15,23 @@ const tables = {
       <table class="table">
   <thead>
     <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
+      <th scope="col">Id</th>
+      <th scope="col">Name</th>
+      <th scope="col">Email</th>
+      <th scope="col">Phone</th>
+      <th scope="col">Action</th>
     </tr>
   </thead>
   <tbody>
     <tr>
       <th scope="row">1</th>
       <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
+      <td>Otto@mail.com</td>
+      <td>01457890</td>
+      <td>
+        <a><button className='btn btn-primary'><i class="bi bi-pencil-square"></i></button></a>
+        <a><button className='btn btn-danger'><i class="bi bi-trash "></i></button></a>
+      </td>
     </tr>
   </tbody>
 </table>
