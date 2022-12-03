4f743dab5a6703b75b14b39dec6edb275ba3f817
unknown <yeasin18801@gmail.com>
2022-12-04 01:23:56 +0600

    fetch 10 use navigate

--- src/component/fetch_curd/FormData.js
+++ src/component/fetch_curd/FormData.js
@@ -1,4 +1,5 @@
 import React,{useState,useEffect} from 'react'
+import { useNavigate } from "react-router-dom";
 
 
 
@@ -6,6 +7,7 @@ const FormData = () => {
   const [name,setName] = useState("");
   const [email,setEmail] = useState("");
   const [phone,setPhone] = useState("");
+  const navigate = useNavigate();
   // const [id,setId] = useState((_.uniqueId("prefix-")));
   const data = {name,email,phone};
 
@@ -24,6 +26,10 @@ const FormData = () => {
       
   // },[])
 
+  // const idGn = (id) =>{
+  //   id = Math.random().toString(36).slice(2);
+  // }
+
   const formHandler = (e) =>{
     e.preventDefault();
     fetch("http://localhost:8000/students",{
@@ -33,23 +39,25 @@ const FormData = () => {
     },
     body: JSON.stringify(data)
   })
-  .catch(error=>{alert(error)})
+  .catch(error=>{alert(error)});
+    navigate("/data-table");
   }
 
   return (
     <div style={{display:"flex",justifyContent:"center", marginTop:"200px", margin:"auto"}}>
       <form onSubmit={formHandler}>
+        {/* <input type="hidden"  onChange={()=>{idGn(id)}}/> */}
           <div className="mb-3">
             <label htmlFor="exampleInputEmail1" className="form-label">Name</label>
-            <input type="text" className="form-control" name='name' onChange={(e)=>setName(e.target.value)}/>
+            <input type="text" className="form-control" value={name} name='name' onChange={(e)=>setName(e.target.value)}/>
           </div>
           <div className="mb-3">
             <label htmlFor="exampleInputEmail1" className="form-label">Mail</label>
-            <input type="text" className="form-control" name='email' onChange={(e)=>setEmail(e.target.value)} />
+            <input type="text" className="form-control" value={email} name='email' onChange={(e)=>setEmail(e.target.value)} />
           </div>
           <div className="mb-3">
             <label htmlFor="exampleInputEmail1" className="form-label">Phone</label>
-            <input type="text" className="form-control" name='phone' onChange={(e)=>setPhone(e.target.value)}/>
+            <input type="text" className="form-control" value={phone} name='phone' onChange={(e)=>setPhone(e.target.value)}/>
           </div>
           <button type="submit" className="btn btn-primary">Submit</button>
         </form>
