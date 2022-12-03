56398243691485a1f3ce4f5d4e5220f49bc7e159
unknown <yeasin18801@gmail.com>
2022-12-03 13:16:25 +0600

    fetch 9 form 2

--- src/component/fetch_curd/FormData.js
+++ src/component/fetch_curd/FormData.js
@@ -2,8 +2,22 @@ import React from 'react'
 
 const FormData = () => {
   return (
-    <div>
-      Form
+    <div style={{display:"flex",justifyContent:"center", marginTop:"200px", margin:"auto"}}>
+      <form>
+          <div className="mb-3">
+            <label htmlFor="exampleInputEmail1" className="form-label">Name</label>
+            <input type="text" className="form-control" />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="exampleInputEmail1" className="form-label">Mail</label>
+            <input type="text" className="form-control" />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="exampleInputEmail1" className="form-label">Phone</label>
+            <input type="text" className="form-control" />
+          </div>
+          <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
     </div>
   )
 }
