094f24bdc590574da5fee2702984401b9103c592
unknown <yeasin18801@gmail.com>
2022-12-05 17:46:01 +0600

    fetch api 11 edit form 3

--- src/component/fetch_curd/Edit.js
+++ src/component/fetch_curd/Edit.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-// import { useParams, useNavigate } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 
 const Edit = () => {
   // const {id} = useParams();
