5055ea6d1ee3c3d6cccbe1daaab350e1dab165d1
unknown <yeasin18801@gmail.com>
2022-12-08 16:12:46 +0600

    Image setup 7

--- src/component/image/ImageForms.js
+++ src/component/image/ImageForms.js
@@ -1,12 +1,15 @@
 import React from 'react'
 
 const ImageForms = () => {
+const formData = new FormData();
+
   return (
     <div>
       <form>
         <div className="form-group" style={{ marginTop:'100px',marginLeft:'100px' }}>
             <label for="exampleFormControlFile1">Example file input</label>
             <input type="file" className="form-control-file" id="exampleFormControlFile1" />
+            <input type="submit" className="form-control-file" id="exampleFormControlFile1" value="Submit" />
         </div>
     </form>
     </div>
