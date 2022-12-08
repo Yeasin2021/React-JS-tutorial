69c04e3cc0907fa6e3f21cabfcc1322193137055
unknown <yeasin18801@gmail.com>
2022-12-08 17:35:24 +0600

    Image setup 10

--- src/component/image/ImageForms.js
+++ src/component/image/ImageForms.js
@@ -1,20 +1,35 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const ImageForms = () => {
-const formData = new FormData();
+const [image,setImage] = useState('');
+const data = {image};
+// const formData = new FormData();
 const inputImage = (e) =>{
-    formData.append('file',e.target.files[0]);
+    
+
+    console.log(e.target.files[0])
+    // console.log(e.target.files[0].name)
+
+    // formData.append('file',e.target.files[0].name);
 }
 const formSubmit = (e) =>{
     e.preventDefault();
-    fetch()
+    fetch("http://localhost:8000/post",{
+        method:"POST",
+        headers:{
+            "content-type":"application/json"
+        },
+        body:JSON.stringify(data)
+    })
+    .then(response => console.log(response))
+    .catch(error=>console.warn("Error: " + error))
 }
   return (
     <div>
-      <form>
+      <form onSubmit={formSubmit}>
         <div className="form-group" style={{ marginTop:'100px',marginLeft:'100px' }}>
-            <label for="exampleFormControlFile1">Example file input</label>
-            <input type="file" className="form-control-file" onChange={inputImage} />
+            <label htmlFor="exampleFormControlFile1">Example file input</label>
+            <input type="file" className="form-control-file" onChange={(e)=>setImage(e.target.files[0].name)} />
             <input type="submit" className="form-control-file" id="exampleFormControlFile1" value="Submit" />
         </div>
     </form>
