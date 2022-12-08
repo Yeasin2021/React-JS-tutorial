5977915a8d74d8f9fe749e2fb59db6b87e91df67
unknown <yeasin18801@gmail.com>
2022-12-08 16:17:35 +0600

    Image setup 8

--- src/component/image/ImageForms.js
+++ src/component/image/ImageForms.js
@@ -2,13 +2,19 @@ import React from 'react'
 
 const ImageForms = () => {
 const formData = new FormData();
-
+const inputImage = (e) =>{
+    formData.append('file',e.target.files[0]);
+}
+const formSubmit = (e) =>{
+    e.preventDefault();
+    
+}
   return (
     <div>
       <form>
         <div className="form-group" style={{ marginTop:'100px',marginLeft:'100px' }}>
             <label for="exampleFormControlFile1">Example file input</label>
-            <input type="file" className="form-control-file" id="exampleFormControlFile1" />
+            <input type="file" className="form-control-file" onChange={inputImage} />
             <input type="submit" className="form-control-file" id="exampleFormControlFile1" value="Submit" />
         </div>
     </form>
