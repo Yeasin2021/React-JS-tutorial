cf1a07d0a423cf2187e26d46f6c32dd91ca9830d
unknown <yeasin18801@gmail.com>
2022-12-08 15:58:01 +0600

    Image setup 2

--- src/component/image/Form.js
+++ src/component/image/Form.js
@@ -4,6 +4,12 @@ const Form = () => {
   return (
     <div>
       <h1>This is Form of Image</h1>
+      <form>
+        <div class="form-group">
+            <label Htmlfor="exampleFormControlFile1">Example file input</label>
+            <input type="file" class="form-control-file" id="exampleFormControlFile1" />
+        </div>
+    </form>
     </div>
   )
 }
