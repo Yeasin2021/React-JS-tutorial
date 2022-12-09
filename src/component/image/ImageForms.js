9f63ddfc43075150930580565e9472d69740628d
unknown <yeasin18801@gmail.com>
2022-12-09 23:40:48 +0600

    Image setup 10 remove 1

--- src/component/image/ImageForms.js
+++ src/component/image/ImageForms.js
@@ -14,12 +14,15 @@ const inputImage = (e) =>{
 }
 const formSubmit = (e) =>{
     e.preventDefault();
+    // const formData = new FormData();
+    // formData.append('photo',image,image.name);
     fetch("http://localhost:8000/post",{
         method:"POST",
         headers:{
             "content-type":"application/json"
         },
         body:JSON.stringify(data)
+        // body:JSON.stringify(formData)
     })
     .then(response => console.log(response))
     .catch(error=>console.warn("Error: " + error))
@@ -30,6 +33,7 @@ const formSubmit = (e) =>{
         <div className="form-group" style={{ marginTop:'100px',marginLeft:'100px' }}>
             <label htmlFor="exampleFormControlFile1">Example file input</label>
             <input type="file" className="form-control-file" onChange={(e)=>setImage(e.target.files[0].name)} />
+            {/* <input type="file" className="form-control-file" name='photo' onChange={(e)=>setImage(e.target.files[0])} /> */}
             <input type="submit" className="form-control-file" id="exampleFormControlFile1" value="Submit" />
         </div>
     </form>
