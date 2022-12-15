eb97532e3d18ec61f61ba688a61591ba13c42dd8
unknown <yeasin18801@gmail.com>
2022-12-15 23:41:28 +0600

    useRef 4

--- src/component/useRef/UserForm.js
+++ src/component/useRef/UserForm.js
@@ -1,12 +1,13 @@
-import React from 'react'
+import React,{useRef} from 'react'
 
 const UserForm = () => {
 const colors = {color:"red"}
+const userName = useRef();
   return (
     <div>
       <form>
             <h1 style={colors}>User name</h1>
-            <input type="text" name="username" />
+            <input type="text" name="username" ref={userName}/>
             <button type='submit' className='btn btn-primary' style={{ marginLeft:'10px' }}>Submit</button>
       </form>
     </div>
