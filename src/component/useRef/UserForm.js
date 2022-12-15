e0879a2a1279b3fe79f95bc40a4c3b670d439556
unknown <yeasin18801@gmail.com>
2022-12-15 23:39:47 +0600

    useRef 3

--- src/component/useRef/UserForm.js
+++ src/component/useRef/UserForm.js
@@ -6,6 +6,8 @@ const colors = {color:"red"}
     <div>
       <form>
             <h1 style={colors}>User name</h1>
+            <input type="text" name="username" />
+            <button type='submit' className='btn btn-primary' style={{ marginLeft:'10px' }}>Submit</button>
       </form>
     </div>
   )
